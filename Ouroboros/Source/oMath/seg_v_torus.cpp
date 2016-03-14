84a9e92dc8c4a0d6b4a058d89a52dcf2e390296e
igHunterKiller <arciuolo@gmail.com>
2016-03-13 23:12:23 -0700

    replace seg_v_torus implementation with something simpler

--- Ouroboros/Source/oMath/seg_v_torus.cpp
+++ Ouroboros/Source/oMath/seg_v_torus.cpp
@@ -6,104 +6,60 @@
 #include <oMath/polynomial.h>
 #include <algorithm>
 
-// Cychosz, Joseph M., Intersecting a Ray with An Elliptical Torus, Graphics Gems II, p. 251-256
-// http://tog.acm.org/resources/GraphicsGems/gemsii/intersect/
-static int inttor(const float3& raybase, const float3& raycos, const float3& center, float radius, float rplane, float rnorm, const float4x4& tran, int* nhits, float rhits[4])
-{
-	int	hit;			/* True if ray intersects torus	*/
-	float rsphere;		/* Bounding sphere radius	*/
-	float3 Base, Dcos;		/* Transformed intersection ray	*/
-	float rmin, rmax;		/* Root bounds			*/
-	double yin, yout;
-	double rho, a0, b0;		/* Related constants		*/
-	double f, l, t, g, q, m, u;	/* Ray dependent terms		*/
-	double C[5];			/* Quartic coefficients		*/
-	double dhits[4];
-
-	*nhits  = 0;
-
-/*	Compute the intersection of the ray with a bounding sphere.	*/
-
-	rsphere = radius + max (rplane,rnorm);
-
-	hit = ouro::ray_v_sphere(raybase, raycos, float4(center, rsphere), &rmin, &rmax);
-
-	if  (!hit) return (hit);	/* If ray misses bounding sphere*/
-
-/*	Transform the intersection ray					*/
-
-	Base = raybase;
-	Dcos = raycos;
-
-	Base = mul(tran, float4(Base, 1.0f)).xyz(); //V3MulPointByMatrix  (&Base,&tran);
-	Dcos = mul((float3x3)tran, Dcos); //V3MulVectorByMatrix (&Dcos,&tran);
-
-/*	Bound the torus by two parallel planes rnorm from the x-z plane.*/
-
-	yin  = Base.y + rmin * Dcos.y;
-	yout = Base.y + rmax * Dcos.y;
-	hit  = !( (yin >  rnorm && yout >  rnorm) ||
-		  (yin < -rnorm && yout < -rnorm) );
-
-	if  (!hit) return (hit);	/* If ray is above/below torus.	*/
-
-/*	Compute constants related to the torus.				*/
-
-	rho = rplane*rplane / (rnorm*rnorm);
-	a0  = 4.0f * radius*radius;
-	b0  = radius*radius - rplane*rplane;
-
-/*	Compute ray dependent terms.					*/
-
-	f = 1.0f - Dcos.y*Dcos.y;
-	l = 2.0f * (Base.x*Dcos.x + Base.z*Dcos.z);
-	t = Base.x*Base.x + Base.z*Base.z;
-	g = f + rho * Dcos.y*Dcos.y;
-	q = a0 / (g*g);
-	m = (l + 2.0f*rho*Dcos.y*Base.y) / g;
-	u = (t +    rho*Base.y*Base.y + b0) / g;
-
-/*	Compute the coefficients of the quartic.			*/
-
-	C[4] = 1.0f;
-	C[3] = 2.0f * m;
-	C[2] = m*m + 2.0f*u - q*f;
-	C[1] = 2.0f*m*u - q*l;
-	C[0] = u*u - q*t;
-	
-/*	Use quartic root solver found in "Graphics Gems" by Jochen	*/
-/*	Schwarze.							*/
-
-	*nhits = ouro::quartic(C[0], C[1], C[2], C[3], C[4], dhits);
-	for (int i = 0; i < *nhits; i++)
-		rhits[i] = (float)dhits[i];
-
-/*	SolveQuartic returns root pairs in reversed order.		*/
-	//m = rhits[0]; u = rhits[1]; rhits[0] = u; rhits[1] = m;
-	//m = rhits[2]; u = rhits[3]; rhits[2] = u; rhits[3] = m;
-
-	return (*nhits != 0);
-}
-
 namespace ouro {
+
+template<typename T> int seg_v_xy_torus(const oHLSL3<T>& p, const oHLSL3<T>& d, T major_radius, T minor_radius, T out_roots[4])
+{
+	// http://www.emeyex.com/site/projects/raytorus.pdf
+
+	const T r2   = minor_radius * minor_radius;
+	const T R2   = major_radius * major_radius;
+	const T a    = dot(d, d);
+	const T b    = dot(p, d) * T(2);
+	const T c    = dot(p, p) - r2 - R2;
+	const T _4R2 = T(4) * R2;
+
+	const T a4   = a * a;
+	const T a3   = a * b * T(2);
+	const T a2   = b * b + T(2) * a * c + _4R2 * d.z * d.z;
+	const T a1   = b * c * T(2) + T(8) * R2 * p.z * d.z;
+	const T a0   = c * c + _4R2 * p.z * p.z - _4R2 * r2;
+
+	return quartic(a0, a1, a2, a3, a4, out_roots);
+}
 
 int seg_v_torus(const float3& a0, const float3& a1, const float3& center, const float3& normalized_axis, float major_radius, float minor_radius, float* at0, float* at1, float* at2, float* at3)
 {
-	// rotate segment into local space (Y up for the original algorithm)
-	float4x4 tx = translate(-center) * rotate(normalized_axis, float3(0.0f, 1.0f, 0.0f));
+	const float3 dir = normalize(a1 - a0);
 
-	float roots[4];
-	int n;
-	if (inttor(a0, normalize(a1-a0), center, major_radius, minor_radius, minor_radius, tx, &n, roots))
+	// check bounding sphere before moving onto more intensive math
+	float rmin, rmax;
+	if (!ray_v_sphere(a0, dir, float4(center, major_radius + minor_radius), &rmin, &rmax))
+		return 0;
+
+	// transform ray into torus local space: xy plane, z-up
+	float4x4 tx = translate(-center) * rotate(normalized_axis, float3(0.0f, 0.0f, 1.0f));
+
+	const float3 local_pos = mul(tx, float4(a0, 1.0f)).xyz();
+	const float3 local_dir = mul((float3x3)tx, dir);
+
+	// early-out if ray doesn't intersect the two XY planes that bound the torus
+	const float zmin = local_pos.z + rmin * local_dir.z;
+	const float zmax = local_pos.z + rmax * local_dir.z;
+	if ((zmin > minor_radius && zmax > minor_radius) || (zmin < -minor_radius && zmax < -minor_radius))
+		return 0;
+
+	double roots[4];
+	int n = seg_v_xy_torus<double>(local_pos, local_dir, major_radius, minor_radius, roots);
+	if (n)
 	{
 		std::sort(roots, roots + n);
 
-		if (n > 0) *at0 = roots[0];
-		if (n > 1) *at1 = roots[1];
-		if (n > 2) *at2 = roots[2];
-		if (n > 3) *at3 = roots[3];
+		if (n > 0) *at0 = (float)roots[0];
+		if (n > 1) *at1 = (float)roots[1];
+		if (n > 2) *at2 = (float)roots[2];
+		if (n > 3) *at3 = (float)roots[3];
 	}
 	return n;
 }
-
 }
